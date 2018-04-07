708b56226f4f67a541afafd3464e58c46490737e
Rafał Chłodnicki <rchl2k@gmail.com>
2018-04-07 18:51:59 +0200

    Ensure left pane opens on the left in diff app

--- ExternalDiff.py
+++ ExternalDiff.py
@@ -17,21 +17,27 @@ def nice_platform_string():
 
 class ExternalDiffCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        view = self.view
-        window = view.window()
+        current_view = self.view
+        window = current_view.window()
 
         '''Find other buffer on the right or left of the current one.'''
         other_group_index = (window.active_group() + 1) % window.num_groups()
         other_view = window.active_view_in_group(other_group_index)
 
-        current_file = view.file_name()
-        other_file = other_view.file_name()
+        left_path = current_view.file_name()
+        right_path = other_view.file_name()
+
+        '''Ensure left view opens on the left in diff view'''
+        current_view_index = window.get_view_index(current_view)
+        other_view_index = window.get_view_index(other_view)
+        if current_view_index > other_view_index:
+            left_path, right_path = right_path, left_path
 
         settings = sublime.load_settings('ExternalDiff.sublime-settings')
         platform_cmd = settings.get(nice_platform_string())
         command = platform_cmd['cmd'] if platform_cmd else settings.get('cmd')
-        command = [c.replace('$file1', current_file) for c in command]
-        command = [c.replace('$file2', other_file) for c in command]
+        command = [c.replace('$file1', left_path) for c in command]
+        command = [c.replace('$file2', right_path) for c in command]
         subprocess.Popen(command, stdout=None, stderr=None)
 
     def is_enabled(self):
