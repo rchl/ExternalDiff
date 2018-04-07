d897dd9cef87fd7f24bce992d73a999b23af5ef1
Rafał Chłodnicki <rafal.chlodnicki@tidal.com>
2018-04-07 17:31:11 +0200

    Remove default keyboard shortcuts
    Also:
      - Change indentation from 2 to 4 spaces.
      - Expand radme.
      - Clean up package menu items.

--- ExternalDiff.py
+++ ExternalDiff.py
@@ -5,51 +5,53 @@ import sys
 
 
 def nice_platform_string():
-  if sys.platform == 'darwin':
-    return 'mac'
-  elif sys.platform in ('windows', 'cygwin'):
-    return 'win'
-  else:
-    return 'linux'
+    if sublime.platform() == 'osx':
+        return 'mac'
+    elif sublime.platform() == 'windows':
+        return 'win'
+    elif sublime.platform() == 'linux':
+        return 'linux'
+    else:
+        raise Exception('Unsupported platform')
 
 
 class ExternalDiffCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    view = self.view
-    window = view.window()
+    def run(self, edit):
+        view = self.view
+        window = view.window()
 
-    '''Find other buffer on the right or left of the current one.'''
-    other_group_index = (window.active_group() + 1) % window.num_groups()
-    other_view = window.active_view_in_group(other_group_index)
+        '''Find other buffer on the right or left of the current one.'''
+        other_group_index = (window.active_group() + 1) % window.num_groups()
+        other_view = window.active_view_in_group(other_group_index)
 
-    current_file = view.file_name()
-    other_file = other_view.file_name()
+        current_file = view.file_name()
+        other_file = other_view.file_name()
 
-    settings = sublime.load_settings('ExternalDiff.sublime-settings')
-    platform_cmd = settings.get(nice_platform_string())
-    command = platform_cmd['cmd'] if platform_cmd else settings.get('cmd')
-    command = [c.replace('$file1', current_file) for c in command]
-    command = [c.replace('$file2', other_file) for c in command]
-    subprocess.Popen(command, stdout=None, stderr=None)
+        settings = sublime.load_settings('ExternalDiff.sublime-settings')
+        platform_cmd = settings.get(nice_platform_string())
+        command = platform_cmd['cmd'] if platform_cmd else settings.get('cmd')
+        command = [c.replace('$file1', current_file) for c in command]
+        command = [c.replace('$file2', other_file) for c in command]
+        subprocess.Popen(command, stdout=None, stderr=None)
 
-  def is_enabled(self):
-    view = self.view
-    window = view.window()
+    def is_enabled(self):
+        view = self.view
+        window = view.window()
 
-    '''Return if file does not psychically exist on disk or is modified.'''
-    if not view.file_name() or view.is_dirty():
-      return False
+        '''Return if file does not psychically exist on disk or is modified.'''
+        if not view.file_name() or view.is_dirty():
+            return False
 
-    '''At least two groups required.'''
-    if window.num_groups() < 2:
-      return False
+        '''At least two groups required.'''
+        if window.num_groups() < 2:
+            return False
 
-    '''Find other buffer on the right or left of the current one.'''
-    other_group_index = (window.active_group() + 1) % window.num_groups()
-    other_view = window.active_view_in_group(other_group_index)
+        '''Find other buffer on the right or left of the current one.'''
+        other_group_index = (window.active_group() + 1) % window.num_groups()
+        other_view = window.active_view_in_group(other_group_index)
 
-    '''Return if file does not psychically exist on disk or is modified.'''
-    if not other_view or not other_view.file_name() or other_view.is_dirty():
-      return False
+        '''Return if file does not psychically exist on disk or is modified.'''
+        if not other_view or not other_view.file_name() or other_view.is_dirty():
+            return False
 
-    return True
+        return True
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-External Diff
-=============
+# External Diff
 
 Sublime Text plugin. Provides a way to trigger diff tool between two adjacent panes. Configuration file allows specifying platform specific command to invoke.
+
+## Configuration
+
+Package does not define any keyboard shortcuts by default. Add your own in user's keymap or use Command Palette to invoke `External Diff` command.
+
+Use package settings to define the command to use when invoking diff. Default settings use p4merge.
+
+### Suggested keyboard shortcuts:
+
+Mac: `{ "keys": ["super+shift+c"], "command": "external_diff" }`
+
+Other platforms: `{ "keys": ["ctrl+shift+c"], "command": "external_diff" }`
